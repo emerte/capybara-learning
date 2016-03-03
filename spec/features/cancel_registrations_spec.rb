b6aab4787b9fcd7d33c47331d0df0f550c89c811
Marcin Wernecki <mwernecki@gmail.com>
2016-03-03 15:54:22 +0100

    something

--- spec/features/cancel_registrations_spec.rb
+++ spec/features/cancel_registrations_spec.rb
@@ -7,13 +7,27 @@ RSpec.feature "Cancel Registration", type: :feature do
 
   before do
     # Sign in
-
+    visit('/users/sign_in')
+    fill_in('Email', with: email)
+    fill_in('Password', with: password)
+    find_button('Sign in').click
+    visit('/users/edit')
+    find_link('Cancel my account').click
     # Cancel registration
     # NOTE: Poltergeist ignore js alert/confirm dialogs
   end
 
-  it "displays a goodbye message"
-
+  it "displays a goodbye message" do
+    expect(page).to have_content("Bye! Your account has been successfully cancelled. We hope to see you again soon.
+")
+  end
   # Try to sign in again
-  it "does not allow the user to sign in again"
+  it "does not allow the user to sign in again" do
+    visit('/users/sign_in')
+    fill_in('Email', with: email)
+    fill_in('Password', with: password)
+    find_button('Sign in').click
+    expect(page).to have_content("Invalid email or password.
+")
+  end
 end
--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -8,25 +8,48 @@ RSpec.feature "User Registration", type: :feature do
 
   context "failure" do
     before do
+      #fill_in("Email", with: "")
+      fill_in("Password", with: "password")
+      fill_in("Password confirmation", with: "password")
+      find_button("Sign up").click
       # try to sign up without an email
     end
 
-    it "displays an error message"
+    it "displays an error message" do
+      expect(page).to have_content("Invalid email or password.
+")
+    end
 
     # should still see 'Sign in' and 'Sign up'
     # should not see 'Profile' or 'Sign out'
-    it "shows the correct navigation links"
+    it "shows the correct navigation links" do
+      expect(page).to have_submit_button("Sign in")
+      expect(page).to have_submit_button("Sign up")
+      expect(page).not_to have_submit_button("Profile")
+      expect(page).not_to have_submit_button("Sign out")
+    end
   end
 
   context "success" do
     before do
+      fill_in("Email", with: "mwernecki@gmail.com")
+      fill_in("Password", with: "password")
+      find_button("Sign up").click
       # sign up successfully
     end
 
-    it "displays a welcome message"
+    it "displays a welcome message" do
+      expect(page).to have_content("Signed in successfully.
+")
+    end
 
     # should not see 'Sign in' and 'Sign up'
     # should see 'Profile' or 'Sign out'
-    it "shows the correct navigation links"
+    it "shows the correct navigation links" do
+      expect(page).not_to have_submit_button("Sign in")
+      expect(page).not_to have_submit_button("Sign up")
+      expect(page).to have_submit_button("Profile")
+      expect(page).to have_submit_button("Sign out")
+    end
   end
 end
