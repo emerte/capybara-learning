7988523a086ccd539da0411731544ba381481e89
Marcin Wernecki <mwernecki@gmail.com>
2016-03-03 16:37:29 +0100

    3/4

--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -8,48 +8,47 @@ RSpec.feature "User Registration", type: :feature do
 
   context "failure" do
     before do
-      #fill_in("Email", with: "")
-      fill_in("Password", with: "password")
-      fill_in("Password confirmation", with: "password")
+      fill_in("Email", with: "")
+      fill_in("user[password]", with: "password")
+      fill_in("user[password_confirmation]", with: "password")
       find_button("Sign up").click
       # try to sign up without an email
     end
 
     it "displays an error message" do
-      expect(page).to have_content("Invalid email or password.
-")
+      expect(page).to have_content("Email can't be blank")
     end
 
     # should still see 'Sign in' and 'Sign up'
     # should not see 'Profile' or 'Sign out'
     it "shows the correct navigation links" do
-      expect(page).to have_submit_button("Sign in")
-      expect(page).to have_submit_button("Sign up")
-      expect(page).not_to have_submit_button("Profile")
-      expect(page).not_to have_submit_button("Sign out")
+      expect(page).to have_content("Sign in")
+      expect(page).to have_content("Sign up")
+      expect(page).not_to have_content("Profile")
+      expect(page).not_to have_content("Sign out")
     end
   end
 
   context "success" do
     before do
       fill_in("Email", with: "mwernecki@gmail.com")
-      fill_in("Password", with: "password")
+      fill_in("user[password]", with: "password")
+      fill_in("user[password_confirmation]", with: "password")
       find_button("Sign up").click
       # sign up successfully
     end
 
     it "displays a welcome message" do
-      expect(page).to have_content("Signed in successfully.
-")
+      expect(page).to have_content("You have signed up successfully.")
     end
 
     # should not see 'Sign in' and 'Sign up'
     # should see 'Profile' or 'Sign out'
     it "shows the correct navigation links" do
-      expect(page).not_to have_submit_button("Sign in")
-      expect(page).not_to have_submit_button("Sign up")
-      expect(page).to have_submit_button("Profile")
-      expect(page).to have_submit_button("Sign out")
+      expect(page).not_to have_content("Sign in")
+      expect(page).not_to have_content("Sign up")
+      expect(page).to have_content("Profile")
+      expect(page).to have_content("Sign out")
     end
   end
 end
