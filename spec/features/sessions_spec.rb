11f303f1c86547e6e819ac27df34b3dabc7cc8da
Marcin Wernecki <mwernecki@gmail.com>
2016-03-03 16:50:16 +0100

    4/4

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -12,25 +12,46 @@ RSpec.feature "User Sessions", type: :feature do
 
   context "failure" do
     before do
+      fill_in("Email", with: email)
+      fill_in("user[password]", with: "")
+      find_button("Sign in").click
       # try to sign in without a password
     end
 
-    it "displays an error message"
+    it "displays an error message" do
+      expect(page).to have_content("Invalid email or password.")
+    end
+
 
     # should still see 'Sign in' and 'Sign up'
     # should not see 'Profile' or 'Sign out'
-    it "shows the correct navigation links"
+    it "shows the correct navigation links" do
+      expect(page).to have_content("Sign in")
+      expect(page).to have_content("Sign up")
+      expect(page).not_to have_content("Profile")
+      expect(page).not_to have_content("Sign out")
+    end
   end
 
   context "success" do
     before do
+      fill_in("Email", with: email)
+      fill_in("user[password]", with: "password")
+      find_button("Sign in").click
       # sign in
     end
 
-    it "displays a welcome message"
+    it "displays a welcome message" do
+      expect(page).to have_content("Signed in successfully.")
+    end
 
     # should not see 'Sign in' and 'Sign up'
     # should see 'Profile' or 'Sign out'
-    it "shows the correct navigation links"
+    it "shows the correct navigation links" do
+      expect(page).not_to have_content("Sign in")
+      expect(page).not_to have_content("Sign up")
+      expect(page).to have_content("Profile")
+      expect(page).to have_content("Sign out")
+    end
   end
 end
