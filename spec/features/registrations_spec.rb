6864889524e74700ad545abfe7b06cc727a303be
Marcin Wernecki <mwernecki@gmail.com>
2016-03-04 10:15:31 +0100

    11/11 z fixikiem

--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -22,10 +22,16 @@ RSpec.feature "User Registration", type: :feature do
     # should still see 'Sign in' and 'Sign up'
     # should not see 'Profile' or 'Sign out'
     it "shows the correct navigation links" do
-      expect(page).to have_content("Sign in")
-      expect(page).to have_content("Sign up")
-      expect(page).not_to have_content("Profile")
-      expect(page).not_to have_content("Sign out")
+      # expect(page).to have_content("Sign in")
+      # expect(page).to have_content("Sign up")
+      # expect(page).not_to have_content("Profile")
+      # expect(page).not_to have_content("Sign out")
+      # find.link('Sign in').visible?
+      #find.link('Sign up').visible?
+      expect(page).to have_link("Sign in")
+      expect(page).to have_link("Sign up")
+      expect(page).not_to have_link("Profile")
+      expect(page).not_to have_link("Sign out")
     end
   end
 
@@ -45,10 +51,10 @@ RSpec.feature "User Registration", type: :feature do
     # should not see 'Sign in' and 'Sign up'
     # should see 'Profile' or 'Sign out'
     it "shows the correct navigation links" do
-      expect(page).not_to have_content("Sign in")
-      expect(page).not_to have_content("Sign up")
-      expect(page).to have_content("Profile")
-      expect(page).to have_content("Sign out")
+      expect(page).not_to have_link("Sign in")
+      expect(page).not_to have_link("Sign up")
+      expect(page).to have_link("Profile")
+      expect(page).to have_link("Sign out")
     end
   end
 end
